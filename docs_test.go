38b52cddec66d9af52059270ce6e27e212ddff3d
Dan Buch <dan@meatballhat.com>
2023-06-24 15:03:01 -0400

    Another newline normalizing

--- docs_test.go
+++ docs_test.go
@@ -204,6 +204,8 @@ func TestToTabularToFileBetweenTags(t *testing.T) {
 	r := require.New(t)
 	r.NoError(fErr)
 
+	expectedDocs = normalizeNewlines(expectedDocs)
+
 	t.Run("default tags", func(t *testing.T) {
 		tmpFile, err := os.CreateTemp("", "")
 
